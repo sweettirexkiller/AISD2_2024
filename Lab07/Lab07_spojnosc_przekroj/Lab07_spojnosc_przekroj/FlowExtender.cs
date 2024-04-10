fcfa7a4a870b5e7c12c7ef6e5aa7c55fd6646946
Piotr Jankiewicz <01128355@pw.edu.pl>
2024-04-10 14:10:09 +0200

    czesc 1 lab7 dziala

--- Lab07/Lab07_spojnosc_przekroj/Lab07_spojnosc_przekroj/FlowExtender.cs
+++ Lab07/Lab07_spojnosc_przekroj/Lab07_spojnosc_przekroj/FlowExtender.cs
@@ -10,6 +10,63 @@ namespace ASD
     public static class FlowExtender
     {
 
+        // metoda zwracajaca przekroj dla danego grafu i danego maksymalnego przeplywu w grafie
+
+        private static Edge<double>[] FindMinCut(Graph<double> g, DiGraph<double> maxFlow, int s)
+        {
+            Edge<double>[] minCut;
+            DiGraph<double> residualGraph = new DiGraph<double>(g.VertexCount);
+            
+            // przejdz po wszystkich wiercholkach 
+            for(int v = 0; v < g.VertexCount; v++)
+            {
+                // na podstweaie przeplywu zbuduj siec rezydualna
+                // jesli przeplyw jest mniejszy od wagi krawedzi to dodaj krawedz do sieci rezydualnej
+                // w przeciwmym kierunku o wartosci rownej przeplywowi i w tym samym kierunku o wartosci rownej wadze krawedzi - przeplyw
+                foreach (Edge<double> e in g.OutEdges(v))
+                {
+                    if (maxFlow.HasEdge(e.From, e.To))
+                    {
+                        if(maxFlow.GetEdgeWeight(e.From, e.To) < e.Weight)
+                        {
+                            residualGraph.AddEdge(e.From, e.To, e.Weight - maxFlow.GetEdgeWeight(e.From, e.To));
+                        }
+                        residualGraph.AddEdge(e.To, e.From, maxFlow.GetEdgeWeight(e.From, e.To));
+
+                    }
+                    else // jesli tej krawedzi nie ma w przeplywie to dodaj ja z waga rowna wadze krawedzi
+                    {
+                        residualGraph.AddEdge(e.From,e.To, e.Weight);
+                    }
+                }
+            }
+            
+            
+            // tablica booli do oznaczania odwiedzonych krawedzi z s i t w sieci rezydualnej 
+            bool[] visited = new bool[g.VertexCount];
+            visited[s] = true;
+            // wykonaj dfs na sieci rezydualnej
+            foreach(Edge<double> e in residualGraph.DFS().SearchFrom(s))
+            {
+                visited[e.To] = true;
+            }
+            
+            // zbuduj tablice krawedzi minimalnego przekroju (znajdz krwaedzie ktore sa w grafie wejsciowym i nie sa odwiedzone)
+            List<Edge<double>> minCutList = new List<Edge<double>>();
+            foreach(Edge<double> e in g.DFS().SearchAll())
+            {
+                if(visited[e.From] && !visited[e.To])
+                {
+                    minCutList.Add(e);
+                }
+            }
+            
+            minCut = minCutList.ToArray();
+            
+            return minCut;
+            
+        }
+
         /// <summary>
         /// Metod wylicza minimalny s-t-przekrój.
         /// </summary>
@@ -23,57 +80,8 @@ namespace ASD
             minCut = null;
             // wykonaj forda fulkersona
             var (flowValue, flow) = Flows.FordFulkerson(undirectedGraph, s, t);
-            
-            Graph<double> residualGraph = new Graph<double>(undirectedGraph.VertexCount);
-            
-            // przejdz po wszystkich wiercholkach 
-            for(int v = 0; v < undirectedGraph.VertexCount; v++)
-            {
-                // na podstweaie przeplywu zbuduj siec rezydualna
-                // jesli przeplyw jest mniejszy od wagi krawedzi to dodaj krawedz do sieci rezydualnej
-                // w przeciwmym kierunku o wartosci rownej przeplywowi i w tym samym kierunku o wartosci rownej wadze krawedzi - przeplyw
-                foreach (Edge<double> e in undirectedGraph.OutEdges(v))
-                {
-                    if (flow.HasEdge(e.From, e.To))
-                    {
-                        if(flow.GetEdgeWeight(e.From, e.To) < e.Weight)
-                        {
-                            residualGraph.AddEdge(e.From, e.To, e.Weight - flow.GetEdgeWeight(e.From, e.To));
-                        
-                        }
-                        residualGraph.AddEdge(e.To, e.From, flow.GetEdgeWeight(e.From, e.To));
+            minCut = FindMinCut(undirectedGraph, flow, s);
 
-                    }
-                    else // jesli tej krawedzi nie ma w przeplywie to dodaj ja z waga rowna wadze krawedzi
-                    {
-                        residualGraph.AddEdge(e.From,e.To, e.Weight);
-                    }
-                }
-            }
-            
-            
-            // tablica booli do oznaczania odwiedzonych krawedzi z s i t w sieci rezydualnej 
-            bool[] visited = new bool[undirectedGraph.VertexCount];
-            visited[s] = true;
-            // wykonaj dfs na sieci rezydualnej
-            foreach(Edge<double> e in residualGraph.DFS().SearchFrom(s))
-            {
-                visited[e.To] = true;
-            }
-            
-            // zbuduj tablice krawedzi minimalnego przekroju (znajdz krwaedzie ktore sa w grafie wejsciowym i nie sa odwiedzone)
-            List<Edge<double>> minCutList = new List<Edge<double>>();
-            foreach(Edge<double> e in undirectedGraph.DFS().SearchAll())
-            {
-                if(visited[e.From] && !visited[e.To])
-                {
-                    minCutList.Add(e);
-                }
-            }
-            
-            minCut = minCutList.ToArray();
-            
-            
             return flowValue;
         }
 
@@ -86,7 +94,20 @@ namespace ASD
         public static int EdgeConnectivity(this Graph<double> undirectedGraph, out Edge<double>[] cutingSet)
         {
             cutingSet = null;
-            return 0;
+            double minSum = double.MaxValue;
+            int s = 0;
+            for(int v = 1; v < undirectedGraph.VertexCount; v++)
+            {
+                var (flowValue, flow) = Flows.FordFulkerson(undirectedGraph, s, v);
+                Edge<double>[] minCutList = FindMinCut(undirectedGraph,flow ,s);
+                double sum = minCutList.Sum(e => e.Weight);
+                if (sum < minSum)
+                {
+                    minSum = sum;
+                    cutingSet = minCutList;
+                }
+            }
+            return minSum == double.MaxValue ? 0 : (int)minSum;
         }
         
     }
