84748f8075c58cfb4d8a70ddaa909189aade2479
Piotr Jankiewicz <01128355@pw.edu.pl>
2024-05-20 20:46:55 +0200

    lab 12 geometria

--- Lab11_geometryczne/Lab11_otoczka_wypukla/Lab11_otoczka_wypukla/Lab11.cs
+++ Lab11_geometryczne/Lab11_otoczka_wypukla/Lab11_otoczka_wypukla/Lab11.cs
@@ -14,6 +14,12 @@ namespace ASD
             double value = (a.Item1 - o.Item1) * (b.Item2 - o.Item2) - (a.Item2 - o.Item2) * (b.Item1 - o.Item1);
             return Math.Abs(value) < 1e-10 ? 0 : value < 0 ? -1 : 1;
         }
+        
+        private double CrossDouble((double, double) o, (double, double) a, (double, double) b)
+        {
+            double value = (a.Item1 - o.Item1) * (b.Item2 - o.Item2) - (a.Item2 - o.Item2) * (b.Item1 - o.Item1);
+            return Math.Abs(value);
+        }
 
         // Etap 1
         // po prostu otoczka wypukła
@@ -22,13 +28,7 @@ namespace ASD
             Stack<(double, double)>  S = Jarvis(points.Distinct().ToArray());
 
             (double, double)[] hull = S.Reverse().ToArray();
-            
-            // console.log(hull);
-            Console.WriteLine();
-            Console.Write("path=[{0}], ", string.Join(",", hull));
-            Console.WriteLine();
-            //
-            
+
             return hull;
         }
 
@@ -55,8 +55,6 @@ namespace ASD
             Stack<(double x, double y)> S = new Stack<(double x, double y)>();
             S.Push(points[0]);
             ((double x, double y) start, (double x,double y) end) line = ((0,0), (1,0));
-            bool[] used = new bool[points.Length];
-            used[0] = true;
 
             while (true)
             {
@@ -67,12 +65,11 @@ namespace ASD
                 double[] vectorL = new double[] { line.end.x - line.start.x, line.end.y - line.start.y };
                 double minAngle = 2 * Math.PI;
                 for(int i = 0; i < points.Length; i++)
-                { 
-                    if(used[i]) continue;
+                {
                     if(points[i] == sTop) continue; 
                     
                     double[] vectorsTopToK = new double[] { points[i].x - sTop.x, points[i].y - sTop.y };
-                  
+                    
                     double angle = FindAngleBetweenVectors(vectorL, vectorsTopToK);
                    if (angle < minAngle)
                    {
@@ -80,40 +77,28 @@ namespace ASD
                        minAngle = angle;
                    } else if( minAngle == angle)
                    {
-                       // jesli kat jest taki sam to wybieramy punkt ktory ma ktorakolwiek wspolrzedna wieksza
-                       if (points[i].x > points[k].x && points[i].y > points[k].y)
+                       // jesli kat jest taki sam to wybieramy punkt ktory jest dalej od sTop
+                       double distToI = Math.Sqrt(Math.Pow(points[i].x - sTop.x, 2) + Math.Pow(points[i].y - sTop.y, 2));
+                       double distToK = Math.Sqrt(Math.Pow(points[k].x - sTop.x, 2) + Math.Pow(points[k].y - sTop.y, 2));
+                       if (distToI > distToK)
                        {
                            k = i;
                        }
                    }
                 }
-                
-                
-                
-                used[k] = true;
+
+               
               
                 if (k == 0)
                 {
                     break;
                 }
-                // line to prosta zawierajaca S.Top() i points[k]
-                line = (S.Peek(), points[k]);
                 
-                // znajdz wszystkie punkty ktore leza na prostej line
-                for (int i = 0; i < points.Length; i++)
-                {
-                    if (used[i]) continue;
-                    if(IsPointOnLine(line.start, line.end, points[i].x, points[i].y))
-                    {
-                        used[i] = true;
-                    }
-                    
-                }
-           
+              
+                line = (S.Peek(), points[k]);
                 S.Push(points[k]);
                 
             }
-
 
             return S;
         }
@@ -149,18 +134,6 @@ namespace ASD
         static double Magnitude(double[] vector)
         {
             return Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
-        }
-        
-        static bool IsPointOnLine((double x,double y) lineStart, (double x,double y) lineEnd, double x, double y)
-        {
-            // Obliczenie współczynnika kierunkowego prostej przechodzącej przez dwa punkty
-            double slope = (lineEnd.y - lineStart.y) / (lineEnd.x - lineStart.x);
-
-            // Obliczenie przesunięcia (intercept) prostej
-            double intercept = lineStart.y - slope * lineStart.x;
-
-            // Sprawdzenie, czy punkt spełnia równanie prostej (y = slope * x + intercept)
-            return Math.Abs(y - (slope * x + intercept)) < 0.0001;
         }
 
     }
---
+++ Lab12_gory_jaskinie_woda/Lab12_woda_gory_jaskinie/Lab12.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASD
+{
+    public class WaterCalculator : MarshalByRefObject
+    {
+
+        /*
+         * Metoda sprawdza, czy przechodząc p1->p2->p3 skręcamy w lewo 
+         * (jeżeli idziemy prosto, zwracany jest fałsz).
+         */
+        private bool leftTurn(Point p1, Point p2, Point p3)
+        {
+            Point w1 = new Point(p2.x - p1.x, p2.y - p1.y);
+            Point w2 = new Point(p3.x - p2.x, p3.y - p2.y);
+            double vectProduct = w1.x * w2.y - w2.x * w1.y;
+            return vectProduct > 0;
+        }
+
+
+        /*
+         * Metoda wyznacza punkt na odcinku p1-p2 o zadanej współrzędnej y.
+         * Jeżeli taki punkt nie istnieje (bo cały odcinek jest wyżej lub niżej), zgłaszany jest wyjątek ArgumentException.
+         */
+        private Point getPointAtY(Point p1, Point p2, double y)
+        {
+            if (p1.y != p2.y)
+            {
+                double newX = p1.x + (p2.x - p1.x) * (y - p1.y) / (p2.y - p1.y);
+                if ((newX - p1.x) * (newX - p2.x) > 0)
+                    throw new ArgumentException("Odcinek p1-p2 nie zawiera punktu o zadanej współrzędnej y!");
+                return new Point(p1.x + (p2.x - p1.x) * (y - p1.y) / (p2.y - p1.y), y);
+            }
+            else
+            {
+                if (p1.y != y)
+                    throw new ArgumentException("Odcinek p1-p2 nie zawiera punktu o zadanej współrzędnej y!");
+                return new Point((p1.x + p2.x) / 2, y);
+            }
+        }
+
+
+        /// <summary>
+        /// Funkcja zwraca tablice t taką, że t[i] jest głębokością, na jakiej znajduje się punkt points[i].
+        /// 
+        /// Przyjmujemy, że pierwszy punkt z tablicy points jest lewym krańcem, a ostatni - prawym krańcem łańcucha górskiego.
+        /// </summary>
+        public double[] PointDepths(Point[] points)
+        {
+            if(points.Length < 3)
+               return new double[points.Length];
+            // Console.WriteLine();
+            // Console.Write("path=[{0}], ", string.Join(",", points.Select(p => $"({p.x},{p.y})").ToArray()));
+            // Console.WriteLine();
+            double[] depths = new double[points.Length];
+            double[] heightsFromLeft = new double[points.Length];
+            double[] heightsFromRight = new double[points.Length];
+            // znajdź wszystkie zagłębienia na terenie, czyli miejsca, gdzie teren jest niższy niż otaczające go obszary
+
+            // przeglądaj kolejne trójkąty utworzone przez sąsiednie wierzchołki na liście
+            Point highestToLeft = points[0];
+            Point highestToRight = points[points.Length - 1];
+
+
+            for (int i = 1; i < points.Length - 1; i++)
+            {
+
+                heightsFromLeft[i] = highestToLeft.y;
+                Point pointFromLeft = points[i];
+
+                if (pointFromLeft.y > highestToLeft.y)
+                {
+                    highestToLeft = pointFromLeft;
+                }
+
+               
+
+            }
+            
+            for(int j = points.Length - 2; j > 0; j--)
+            {
+                heightsFromRight[j] = highestToRight.y;
+                
+                Point pointFromRight = points[j];
+                if (pointFromRight.y > highestToRight.y)
+                {
+                    highestToRight = pointFromRight;
+                }
+            }
+            
+            for (int i = 1; i < points.Length - 1; i++)
+            {
+                if(points[i].y < heightsFromLeft[i] && points[i].y < heightsFromRight[i])
+                {
+                    depths[i] = Math.Min(heightsFromLeft[i], heightsFromRight[i]) - points[i].y;
+                }
+                else
+                {
+                    depths[i] = 0;
+                }
+            }
+
+            
+            return depths;
+    }
+
+        /// <summary>
+        /// Funkcja zwraca objętość wody, jaka zatrzyma się w górach.
+        /// 
+        /// Przyjmujemy, że pierwszy punkt z tablicy points jest lewym krańcem, a ostatni - prawym krańcem łańcucha górskiego.
+        /// </summary>
+        public double WaterVolume(Point[] points)
+        {
+            // wielokaty od glebokosci 0 do 0 
+            
+            // najlepiej obliczyc metoda trapezow ?
+            
+            double[] depths = PointDepths(points);
+            double volume = 0;
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                // jesli trojkat
+                if (depths[i] == 0 && depths[i +1] != 0)
+                {
+                    // najpierw trzeba znalesc punkt przeciecia
+                    Point p1 = points[i];
+                    Point p2 = points[i + 1];
+                    // jaka jest wspolrzedna y wysokosic tafli wody ?
+                    
+                    double y = points[i+1].y + depths[i+1];
+                    Point p3 = getPointAtY(p1, p2, y);
+                    // pole trojkata p3 p1 0 
+                    volume += depths[i + 1] * (points[i + 1].x - p3.x) / 2;
+                } else if(depths[i] != 0 && depths[i + 1] == 0)
+                {
+                    Point p1 = points[i];
+                    Point p2 = points[i + 1];
+                    double y = points[i].y + depths[i];
+                    Point p3 = getPointAtY(p1, p2, y);
+                    
+                    volume += depths[i] * (p3.x - points[i].x) / 2;
+                } else if(depths[i] != 0 && depths[i + 1] != 0) // jesli trapez
+                {
+                    
+                    // przekatna 1 
+                    double[] d1 = new double[2]{ points[i].x - points[i+1].x, 0 - depths[i+1]};
+                    // przekartna 2
+                    double[] d2 = new double[2]{ points[i+1].x - points[i].x, 0 - depths[i]};
+                    // kat miedzy przekatnymi
+                    double angle = Math.Acos((d1[0] * d2[0] + d1[1] * d2[1]) / (Math.Sqrt(d1[0] * d1[0] + d1[1] * d1[1]) * Math.Sqrt(d2[0] * d2[0] + d2[1] * d2[1])));
+                    // pole 
+                    double l1 = Math.Sqrt(d1[0] * d1[0] + d1[1] * d1[1]);
+                    double l2 = Math.Sqrt(d2[0] * d2[0] + d2[1] * d2[1]);
+                    volume += 0.5 * l1 * l2 * Math.Sin(angle);
+                }
+                
+            }
+
+            return volume;
+        }
+    }
+
+    [Serializable]
+    public struct Point
+    {
+        public double x, y;
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
