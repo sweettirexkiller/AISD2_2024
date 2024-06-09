7b136a6fb897ded38cbab90c0d2f12a73c3cbfa8
Piotr Jankiewicz <01128355@pw.edu.pl>
2024-06-09 16:41:02 +0200

    zmiana podejscia

--- Lab15_dowolne_kolorawnie/Lab15_kolorowanie/GraphColorer.cs
+++ Lab15_dowolne_kolorawnie/Lab15_kolorowanie/GraphColorer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ASD.Graphs;
 
 namespace ASD2
@@ -23,6 +24,7 @@ namespace ASD2
 
             bool[] visited = new bool[g.VertexCount];
             bool[] colored = new bool[g.VertexCount];
+            bool[] considerAtEnd = new bool[g.VertexCount];
             int colorsCount = 0;
 
 
@@ -30,8 +32,22 @@ namespace ASD2
             coloring[0] = 0;
             visited[0] = true;
             colorsCount++;
+            bool[] allneighboursColored = new bool[g.VertexCount];
             
-            bool found = FindColoringRec(g, 0,ref coloring,ref colored, ref colorsCount, ref visited);
+            bool found = FindColoringRec(g, 0,ref coloring,ref colored, ref colorsCount, ref visited, ref considerAtEnd, ref allneighboursColored);
+            // sprawdz czy ktorys nieodwiedzony wierzcholek
+            for (int i = 0; i < g.VertexCount; i++)
+            {
+                if (visited[i] == false || colored[i] == false || coloring[i] == -1)
+                {
+                    if (i == 6)
+                    {
+                        int x;
+                    }
+                    found = FindColoringRec(g, i,ref coloring,ref colored, ref colorsCount, ref visited, ref considerAtEnd, ref allneighboursColored);
+                }
+            }
+            
             if (found)
             {
                 return (colorsCount, coloring);
@@ -40,81 +56,165 @@ namespace ASD2
             return (0, null);
         }
         
-        private bool FindColoringRec(Graph g, int v,ref int[] coloring,ref bool[] colored, ref int colorsCount, ref bool[] visited)
+        private bool FindColoringRec(Graph g, int v,ref int[] coloring,ref bool[] colored, ref int colorsCount, ref bool[] visited, ref bool[] considerAtEnd, ref bool[] allneighboursColored)
         {
+            if(g.OutNeighbors(v).Count() == 0)
+            {
+                visited[v] = true;
+                coloring[v] = 0;
+                colored[v] = true;
+                return true;
+            }
+            
+            if(allneighboursColored[v])
+            {
+                // znajdz najmniejszy mozliwy kolor
+                for (int color = 0; color < colorsCount; color++)
+                {
+                    if (IsSafe(g, v, color, coloring))
+                    {
+                        coloring[v] = color;
+                        colored[v] = true;
+                        visited[v] = true;
+                        return true;
+                    }
+                }
+            }
             
             // if all vertices are colored
             if (v == g.VertexCount - 1)
             {
+                // czy udało się pokolorować pozostałe wierzchołki?
+                
+                for (int i = 0; i < g.VertexCount; i++)
+                {
+                    if (considerAtEnd[i] == true)
+                    {
+                        // czy udało się pokolorować pozostałe wierzchołki?
+                        // pokoloruj na najmniejszy mozliwy kolor
+                        considerAtEnd[i] = false;
+                        for (int color = 0; color < colorsCount; color++)
+                        {
+                            if (IsSafe(g, i, color, coloring))
+                            {
+                                coloring[i] = color;
+                                colored[i] = true;
+                                visited[i] = true;
+                                if (FindColoringRec(g, i, ref coloring, ref colored, ref colorsCount, ref visited, ref considerAtEnd, ref allneighboursColored))
+                                {
+                                    return true;
+                                }
+                                colored[i] = false;
+                                visited[i] = false;
+                                coloring[i] = -1;
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+            if(g.OutNeighbors(v).Count() == 0)
+            {
+                visited[v] = true;
+                coloring[v] = 0;
+                colored[v] = true;
                 return true;
             }
             foreach(int w in g.OutNeighbors(v))
             {
+                if (considerAtEnd[w])
+                {
+                    continue;
+                }
                 if (colored[w] == false)
                 {
-                    // ktory kolor wolny ?
-                    bool foundColor = false;
-                    for (int color = 0; color < colorsCount; color++)
+                    // 1. policz ile niepokolororwanych sasiadow
+                    int notColoredNeighbours = 0;
+                    foreach (int neighbour in g.OutNeighbors(w))
                     {
-                        // czy mozna pomalowac w  w kolorze i
-                        if (IsSafe(g, w, color, coloring))
+                        if (!colored[neighbour])
                         {
-                            foundColor = true;
-                            coloring[w] = color;
-                            colored[w] = true;
-                            visited[w] = true;
-                            if (FindColoringRec(g, w, ref coloring, ref colored, ref colorsCount, ref visited))
-                            {
-                                return true;
-                            }
-                            colored[w] = false;
-                            visited[w] = false;
-                            coloring[w] = -1;
+                            notColoredNeighbours++;
                         }
                     }
-
-                    if (!foundColor)
+                    // 2. Policz liczbe dostepnych kolorow dla wierzcholka w
+                    int availableColors = 0;
+                    for (int color = 0; color < colorsCount; color++)
+                    {
+                        if (IsSafe(g, w, color, coloring))
+                        {
+                            availableColors++;
+                        }
+                    }
+                    
+                    // Jeżeli liczba kolorów dostępnych dla jakiegoś wierzchołka w
+                    // jest wieksza niż liczba jego niepokolorowanych sąsiadów,
+                    // możemy usunąć wierzchołek v z dalszych rozważań (i -
+                    // jeżeli udało się pokolorować pozostałe wierzchołki
+                    //  - dokolorować v na końcu)
+                    
+                    if(notColoredNeighbours == 0)
+                    {
+                        allneighboursColored[w] = true;
+                    }
+                    
+                    if (availableColors > notColoredNeighbours && notColoredNeighbours > 0)
+                    {
+                        visited[w] = true;
+                        considerAtEnd[w] = true;
+                        continue;
+                    }
+                    
+                    // ktory kolor wolny ?
+                    if (availableColors == 0)
                     {
                         colorsCount++;
                         coloring[w] = colorsCount - 1;
                         colored[w] = true;
-                        if (FindColoringRec(g, w, ref coloring, ref colored, ref colorsCount, ref visited))
+                        visited[w] = true;
+                        if (FindColoringRec(g, w, ref coloring, ref colored, ref colorsCount, ref visited, ref considerAtEnd, ref allneighboursColored))
                         {
+                            
                             return true;
                         }
                         colorsCount--;
                         colored[w] = false;
+                        visited[w] = false;
                         coloring[w] = -1;
+                        continue;
                     }
-                }
-            }
-            for(int i = 0; i < g.VertexCount; i++)
-            {
-                
-                if (!visited[i])
-                {
-                    for (int color = 0; color < colorsCount; color++)
+                    else
                     {
-                        if (IsSafe(g, i, color, coloring))
+                        for (int color = 0; color < colorsCount; color++)
                         {
-                            coloring[i] = color;
-                            colored[i] = true;
-                            visited[i] = true;
-                            if (FindColoringRec(g, i, ref coloring, ref colored, ref colorsCount, ref visited))
+                            // czy mozna pomalowac w  w kolorze i
+                            if (IsSafe(g, w, color, coloring))
                             {
-                                return true;
+                                coloring[w] = color;
+                                colored[w] = true;
+                                visited[w] = true;
+                                if (allneighboursColored[w] == false)
+                                {
+                                    if (FindColoringRec(g, w, ref coloring, ref colored, ref colorsCount, ref visited, ref considerAtEnd, ref allneighboursColored))
+                                    {
+                                        return true;
+                                    }
+                                    colored[w] = false;
+                                    visited[w] = false;
+                                    coloring[w] = -1;
+                                }
+                                else
+                                {
+                                    break;
+                                }
                             }
-                            colored[i] = false;
-                            visited[i] = false;
-                            coloring[i] = -1;
                         }
                     }
-
-                   
                 }
             }
-            
-            return false;
+            allneighboursColored[v] = true;
+
+            return true;
         }
 
         private bool IsSafe(Graph graph, int vertex, int color, int[] coloring)
